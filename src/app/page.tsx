27bdbb0ba2dea95f0c505be89c3645b72a851dcb
alexandre <alexandre020602@gmail.com>
2025-09-23 18:39:38 -0300

    finalizando dashboardCard Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import DashboardCard from "./Components/DashboardCard/DashboardCard";
+import { Folder, MessageCircle, Newspaper, Users } from 'lucide-react'
 
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
+      <DashboardCard title="Posts" icon={<Newspaper className="text-slate-500" size={60} />} count={100} />
+      <DashboardCard title="Categories" icon={<Folder className="text-slate-500" size={60} />} count={12} />
+      <DashboardCard title="Users" icon={<Users className="text-slate-500" size={60} />} count={750} />
+      <DashboardCard title="Comments" icon={<MessageCircle className="text-slate-500" size={60} />} count={1200} />
     </div>
-   
   );
 }
